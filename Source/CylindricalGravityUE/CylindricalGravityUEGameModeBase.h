ad7313f8717fb87e571ceb0219132631ac72c895
Flynn Ringrose <flynnringrose@gmail.com>
2018-06-16 12:07:28 -0300

    Created project. Set up git.

---
+++ Source/CylindricalGravityUE/CylindricalGravityUEGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CylindricalGravityUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYLINDRICALGRAVITYUE_API ACylindricalGravityUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
